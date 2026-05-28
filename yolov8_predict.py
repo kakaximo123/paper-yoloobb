c19bcbf59c5109ae00f6f81be1e8c60a44980596
UltralyticsAssistant <web@ultralytics.com>
2026-05-28 03:09:52 +0000

    Auto-format by https://ultralytics.com/actions

--- yolov8_predict.py
+++ yolov8_predict.py
@@ -1,7 +1,11 @@
-from ultralytics import YOLO
 import math
+
+from ultralytics import YOLO
+
 # 加载预训练的 YOLOv8n 模型
-model = YOLO(r'D:\examples_project\python projects\self-enhance-all\ultralytics-main-3\ultralytics-main\runs\obb\train65\weights\best.pt')
+model = YOLO(
+    r"D:\examples_project\python projects\self-enhance-all\ultralytics-main-3\ultralytics-main\runs\obb\train65\weights\best.pt"
+)
 # model = YOLO('yolov8n.pt')
 # 定义图像文件的路径
 # source = (r'"D:\examples_project\python projects\ultralytics-main\ultralytics-main\camera\photo\capture_20250911_150809.jpg"') #更改为自己的图片路径
